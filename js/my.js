31fadf9790d50edcca6031718311fe6df286c16d
lalit singh <lalit92bora@gmail.com>
2022-04-30 16:55:24 +0530

    Seventh Commit

--- js/my.js
+++ js/my.js
@@ -41,3 +41,20 @@ $(document).ready(function () {
     }
 });
 });
+
+$(document).ready(function () {
+  $('.invested_listed').css("display", "none");
+  $('.investlisting_item_one').click(function(){
+  $('.investlisting_item_one').addClass('active'); 
+  $('.investlisting_item_two').removeClass('active');
+  $('.investlisting_box_parent').css("display", "grid");
+  $('.invested_listed').css("display", "none");
+});
+
+$('.investlisting_item_two').click(function(){
+  $('.investlisting_item_two').addClass('active'); 
+  $('.investlisting_item_one').removeClass('active');
+  $('.invested_listed').css("display", "block");
+  $('.investlisting_box_parent').css("display", "none");
+});
+});
